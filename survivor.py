4eed8297226f96af4a4a964fac3efe5dd008b618
Pekka Tiilikainen <pekka.tiilikainen@digia.com>
2017-03-03 15:54:52 +0200

    Changes board from dictionaries to vectors

--- survivor.py
+++ survivor.py
@@ -1,24 +1,25 @@
-board = {'a': {'1': 1, '2': 1, '3': 1, '4': 1},
-         'b': {'1': 1, '2': 1, '3': 1, '4': 1},
-         'c': {'1': 1, '2': 1, '3': 1, '4': 1},
-         'd': {'1': 1, '2': 1, '3': 1, '4': 1}}
+board = [[1, 1, 1, 1],
+         [1, 1, 1, 1],
+         [1, 1, 1, 1],
+         [1, 1, 1, 1]]
 
 gameOver=False
+gameStart=True
+wizardPosition=[4,4]
+magePosition=[4,4]
+
+def legalMovesWizard():
+    if gameStart is True:
+        return []
+    else:
+        return []
 
 def drawBoard():
-    print '  1  2  3  4'
     for row in board:
-        drawnRow = row
-        for square in board[row]:
-            if board[row][square] == 1:
-                drawnRow = drawnRow + ' X '
-            else:
-                drawnRow = drawnRow + ' O '
-        print drawnRow
+        print row
 
 def moveWizard():
     move=raw_input("Where to move Wizard?")
-
 
 def moveMage():
     move=raw_input("Where to move Mage?")
