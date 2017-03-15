29ea570816dd84972e04ee6b0073c881a3077dbe
Pekka Tiilikainen <pekka.tiilikainen@digia.com>
2017-03-15 17:19:30 +0200

    Adds moving and drawing for characters
    Changed moving to be general for given char.

--- survivor.py
+++ survivor.py
@@ -3,11 +3,6 @@ board = [[1, 1, 1, 1],
          [1, 1, 1, 1],
          [1, 1, 1, 1]]
 
-gameOver=False
-gameStart=True
-wizardPosition=[4,4]
-magePosition=[4,4]
-
 def legalMovesWizard():
     if gameStart is True:
         return []
@@ -18,13 +13,45 @@ def drawBoard():
     for row in board:
         print row
 
-def moveWizard():
-    move=raw_input("Where to move Wizard?")
+def findChar(char):
+    for index, val in enumerate(board):
+        locationX = index
+        try:
+            locationY = val.index(char)
+            return [locationX, locationY]
+        except ValueError:
+            pass
 
-def moveMage():
-    move=raw_input("Where to move Mage?")
+def changeBoard(position, char):
+    position = position.split(',')
+    posX = int(position[0])
+    posY = int(position[1])
+    board[posX][posY] = char
 
-while not gameOver:
-    moveWizard()
-    moveMage()
+def moveChar(char):
+    if char=='W':
+        move=raw_input("Where to move Wizard?")
+    elif char=='M':
+        move=raw_input("Where to move Mage?")
+    curPos = findChar(char)
+    board[curPos[0]][curPos[1]] = 0
+    madeMove = move.split(',')
+    if board[int(madeMove[0])][int(madeMove[1])] == 0:
+        print char + ' Lost the game!'
+        #TODO: Call ending game here
+    changeBoard(move, char)
+
+gameOn=True
+drawBoard()
+move=raw_input("Where does the Wizard start?")
+changeBoard(move, 'W')
+drawBoard()
+move=raw_input("Where does the Mage start?")
+changeBoard(move, 'M')
+drawBoard()
+
+while gameOn:
+    moveChar('W')
+    drawBoard()
+    moveChar('M')
     drawBoard()
